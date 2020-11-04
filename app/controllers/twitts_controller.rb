1f16cf5fd724c3dc6ed296c7d033d2bd5ad203f5
Alexandre Tozzini <alexandre.tozzini.at@gmail.com>
2020-11-03 22:01:43 -0300

    remove action de teste

--- app/controllers/twitts_controller.rb
+++ app/controllers/twitts_controller.rb
@@ -1,15 +1,10 @@
 class TwittsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def index
-    # debugger
-    # appid = ENV['OPEN_WEATHER_FORECAST_KEY']
-    message = TinyWeatherForecast::Forecast.new(params[:city]).forecast
-
-    render json: message
-  end
-
   def twittar
+    appid = ENV['OPEN_WEATHER_FORECAST_KEY']
+    geocoder_key = ENV['GEOCODER_KEY']
+
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_API_KEY']
       config.consumer_secret     = ENV['TWITTER_API_KEY_SECRET']
@@ -18,9 +13,8 @@ class TwittsController < ApplicationController
       config.bearer_token        = ENV['TWITTER_BEARER_TOKEN']
     end
 
-    message = TinyWeatherForecast::Forecast.new(params[:city]).forecast
+    message = TinyWeatherForecast::Forecast.new(params[:city], appid, geocoder_key).forecast
 
-    debugger
     client.update(message)
 
     render json: message
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :twitts, only: :index do
+  resources :twitts, only: :post do
     collection do
       post :twittar
     end
